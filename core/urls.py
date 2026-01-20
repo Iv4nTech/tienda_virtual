340da21ebf59e8a031a9eb7042cdcdc949ccf5ef
Iv4nGomez <xigomjim596@ieshnosmachado.org>
2026-01-20 10:38:25 +0100

    Ver productos y detalle creado

--- core/urls.py
+++ core/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
+from .views import *
 
 urlpatterns = [
-
+    path('tienda/admin/productos', ver_productos.as_view(), name='ver_producto'),
+    path('tienda/admin/productos/<int:pk>', detalle_producto.as_view(), name='detalle_producto')
 ]
--- core/views.py
+++ core/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.views.generic import ListView, CreateView, DetailView
+from .models import *
 
-# Create your views here.
+class ver_productos(ListView):
+    template_name = 'core/ver_productos.html'
+    context_object_name = 'productos'
+    model = Producto
+    
+class detalle_producto(DetailView):
+    model = Producto
+    context_object_name = 'producto'
+    template_name = 'core/detalle_producto.html'
